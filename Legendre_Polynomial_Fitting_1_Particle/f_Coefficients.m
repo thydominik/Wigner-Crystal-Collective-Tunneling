96ce3413e4ac418179e81fa8a0f3c87a3b3d0d44
Dominik Szombathy <szombathy.dominik@gmail.com>
2021-08-02 15:13:26 +0200

    Update f_Coefficients.m

--- Legendre_Polynomial_Fitting_1_Particle/f_Coefficients.m
+++ Legendre_Polynomial_Fitting_1_Particle/f_Coefficients.m
@@ -1,14 +1,17 @@
 function [C] = f_Coefficients(S1,S2, N_Lp)
 %this function is not yet working
+S2_new = zeros(N_Lp, N_Lp);
+
 for i = 1:N_Lp
     for j = 1:N_Lp
-        if i == j
-            S2(i,j) = S2(i,j) * 2;
-        end
+        S2_new(i,j) = S2(i,j) + S2(j,i);
     end
 end
-C = linsolve(S2, -S1');
-C(2:2:end) = 0;
+
+C = linsolve(- S2_new, S1'); 
+%C(2:2:end) = 0;
 C = C';
+
+
 end
 
