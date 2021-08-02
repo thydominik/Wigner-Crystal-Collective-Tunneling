daafa3d18b85fea2f24e789b799bee98a1cbc2f6
Dominik Szombathy <szombathy.dominik@gmail.com>
2021-08-02 15:14:33 +0200

    Update Legendre_polynomials_1p.m

--- Legendre_Polynomial_Fitting_1_Particle/Legendre_polynomials_1p.m
+++ Legendre_Polynomial_Fitting_1_Particle/Legendre_polynomials_1p.m
@@ -3,13 +3,13 @@ clc
 clear all
 tic
 
-N_Lp        = 10;       % number of polynomials
-Iteration   = 100;
-div         = 1000;      % number of points
+N_Lp        = 15;       % number of polynomials
+Iteration   = 10000;
+div         = 400;      % number of points
 r           = 3;        %we can get back a linear traj. with r = (alpha/2)^-(1/2)
 syms x
 eps     = 10^-10;    % only significant for the S_0 calculation, all first and second derivatives are non divergent integrals.
-z   = linspace(-1+eps, 1-eps, div);
+z   = linspace(-1 + 10^-10, 1 - 10^-10, div);
 %z       = linspace(-1, 1, div);
 alpha   = 5;
 
@@ -25,11 +25,12 @@ traj_apr = sqrt(alpha) * tanh(atanh(z)*sqrt((alpha+0.1)/2) * r);
 %well)
 
 for i = 1:N_Lp
-    Leg_sym(i)        = legendreP(i, x);
+    Leg_sym(i)        = (1 - x^2) * legendreP(i, x);
     Dif_Leg_sym(i)    = diff(Leg_sym(i), x);
 end
 Legendre        = eval(subs(Leg_sym',x,z));
 Legendre_diff   = eval(subs(Dif_Leg_sym',x,z));
+
 
 %guess curve (q0)
 [C, q_0, dQ] = f_Guesscurve(alpha, N_Lp, div, r, z);
@@ -50,7 +51,6 @@ xlabel('z')
 ylabel('q_0 (z)(blue) and exact solution(red)')
 hold off
 
-
 %although one could separate some part of the integrals in order to save
 %some time calculating the integrals, but as of now its not that important
 
@@ -62,23 +62,23 @@ for i = 1:Iteration
     S2 = f_int_S2(z, q_0, div, Legendre, Legendre_diff, N_Lp, alpha, r);
     
     %determining the coefficients from S1 and S2:
-    C = f_Coefficients(S1, S2, N_Lp);
+    C_mtx(i,:) = C;
+    C = C + f_Coefficients(S1, S2, N_Lp);
     %calculating the new q_0 trajectory:
     q_0 = f_q_traj(z, C, Legendre, N_Lp, r, alpha);
     
     %calculating the action from the new q_0 traj
     Action(i) = f_Action(q_0, r, alpha, div, z);
-    
-    if mod(i,100) == 0
-       disp(Action(end) - S_0)
-       
-       figure(2)
-       clf(figure(2))
-       hold on
-       plot(q_0)
-       plot(traj)
-       hold off
 
+    if mod(i,1000) == 0
+        disp(Action(end) - S_0)
+        
+        figure(2)
+        clf(figure(2))
+        hold on
+        plot(q_0)
+        plot(traj)
+        hold off
     end
 end
 
@@ -89,4 +89,10 @@ clf(figure(3))
 hold on
 plot(Action)
 
+hold off
+
+figure(4)
+clf(figure(4))
+hold on
+plot(C_mtx)
 hold off
