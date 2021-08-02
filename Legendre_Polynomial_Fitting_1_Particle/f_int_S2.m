b80a9c31a05a7ce74d98ec831a67cbc8a4003851
Dominik Szombathy <szombathy.dominik@gmail.com>
2021-08-02 15:13:49 +0200

    Update f_int_S2.m

--- Legendre_Polynomial_Fitting_1_Particle/f_int_S2.m
+++ Legendre_Polynomial_Fitting_1_Particle/f_int_S2.m
@@ -5,10 +5,8 @@ function [I_S2] = f_int_S2(z, q_0, div, leg, dleg, N_Lp, alpha, r)
     
     for i = 1:N_Lp
         for j = 1:N_Lp
-            L_tilde_i   = (1 - z.^2) .* dleg(i,:) - 2 .* z .* leg(i,:);
-            L_tilde_j   = (1 - z.^2) .* dleg(j,:) - 2 .* z .* leg(j,:);
-            S2_func_p1  = (1 - z.^2)./(2 * r) .* L_tilde_i .* L_tilde_j;
-            S2_func_p2  = (r/2) .* (1 - z.^2) .* leg(i,:) .* leg(j,:) .* (3 .* q_0.^2 - alpha);
+            S2_func_p1  = (1 - z.^2)./(2 * r) .* dleg(i,:) .* dleg(j,:);
+            S2_func_p2  = r./(1 - z.^2) .* 0.5 .* ((3 .* q_0.^2 - alpha) .* leg(i,:) .* leg(j,:));
             S2_func     = S2_func_p1 + S2_func_p2;
             for k = 2:div
                 I_S2(i,j)   = I_S2(i,j) + dz * (S2_func(k) + S2_func(k - 1))/2;                
