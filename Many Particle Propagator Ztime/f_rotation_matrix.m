69703d5ffa862a99d01b34ee08b717707d2886ca
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2022-07-26 10:37:34 +0200

    Delete Many Particle Propagator Ztime directory

--- Many Particle Propagator Ztime/f_rotation_matrix.m
+++
@@ -1,9 +0,0 @@
-function  R = f_rotation_matrix(N, e, f, delta_phi)
-    R = zeros(3,3,N);
-    for i = 1:N
-        R1 = eye(3) - (e(:,i)*e(:,i)' + f(:,i)*f(:,i)') + (e(:,i)*e(:,i)' + f(:,i)*f(:,i)') * cos((delta_phi(i)));
-        R2 = (f(:,i)*e(:,i)' - (e(:,i)*f(:,i)')) * sin((delta_phi(i)));
-        R(:,:,i) =  (R1 + R2);
-    end
-end
-
