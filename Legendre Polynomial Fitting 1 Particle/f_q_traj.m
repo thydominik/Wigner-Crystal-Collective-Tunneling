0bdefd7b547123ec0c400c05160fe9bc66c87a20
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2022-07-26 10:37:18 +0200

    Delete Legendre Polynomial Fitting 1 Particle directory

--- Legendre Polynomial Fitting 1 Particle/f_q_traj.m
+++
@@ -1,8 +0,0 @@
-function [q_0] = f_q_traj(z, C, Legendre, N, r, alpha)
-
-    q_0 = sqrt(alpha) * tanh(atanh(z) * sqrt(alpha/2) * r );
-    for i = 1:N
-        q_0 =  q_0 + (C(i) * Legendre(i,:));
-    end
-end
-
