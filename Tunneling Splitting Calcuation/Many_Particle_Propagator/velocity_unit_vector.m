c2b548b462117b854a9d27f117da7909b416c342
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2022-07-26 10:37:53 +0200

    Delete Tunneling Splitting Calcuation directory

--- Tunneling Splitting Calcuation/Many_Particle_Propagator/velocity_unit_vector.m
+++
@@ -1,9 +0,0 @@
-function [unit_vector] = velocity_unit_vector(velocity)   
-    unit_vector = velocity;
-    
-    for i = 1:length(velocity)
-        normalization = norm(velocity(:,i));        
-        unit_vector(:,i) = velocity(:,i)/normalization;
-    end
-end
-
--- Tunneling Splitting Calcuation/Splitting_Calculation/f_plot.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = f_plot(inputArg1,inputArg2)
-%F_PLOT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
