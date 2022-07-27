997f6c0dce7359ed2a9283ed24406e8f2cbfe0a4
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2022-07-27 13:49:38 +0200

    Fitted Option added

---
+++ Trajectory Calculation/3 particle/TanFitting.m
@@ -0,0 +1,27 @@
+function [GOFMTX] = TanFitting(Pos, NoP, PN, z)
+    for particleInd = 1:PN
+        y = Pos(particleInd, 2:end-1).';
+        x = z(2:end-1).';
+        
+        if particleInd == floor(PN/2) + 1
+            starting_points = [1];
+            b = abs(y(1));
+            fitfun = fittype( @(c, x) b*tanh(atanh(x)* c));
+            [fitted_curve, gof] = fit(x, y, fitfun, 'StartPoint', starting_points);
+            coeffvals = coeffvalues(fitted_curve);
+            disp('illesztés adatai: ')
+            disp(num2str(coeffvals))
+        else
+            starting_points = [0 0 0];
+            b = abs(y(end) - y(1))/2;
+
+            fitfun = fittype( @(a, c, d, x) a + b.*tanh(atanh(x).* c + d));
+            [fitted_curve, gof] = fit(x, y, fitfun, 'StartPoint', starting_points);
+            coeffvals = coeffvalues(fitted_curve);
+            disp('illesztés adatai: ')
+            disp(num2str(coeffvals))
+        end
+        GOFMTX(particleInd, :) = coeffvals;
+    end
+end
+
